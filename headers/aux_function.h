3f582dfdcf3677717185975041c3d224476a952c
gabriele0011 <gabri0896@gmail.com>
2022-10-24 14:20:37 +0200

    pre implementazione pipe

--- headers/aux_function.h
+++ headers/aux_function.h
@@ -7,6 +7,6 @@ long is_number(const char* s);
 int is_opt(char* arg, char* opt);
 int is_argument(char* c);
 int is_directory(const char *path);
-int msleep(long int msec) 
+int msleep(long int msec); 
 void read_n(int fd, long int* buf, size_t bytes);
-void write_n(int fd, long int* buf, size_t bytes)
+void write_n(int fd, long int* buf, size_t bytes);
